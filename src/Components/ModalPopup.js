0375002c3354a05b74f36756d0a07db0b5d441b8
ducthai127 <nguyenducthai127@gmail.com>
2020-10-20 01:04:50 +0700

    add react-checkbox-group

--- src/Components/ModalPopup.js
+++ src/Components/ModalPopup.js
@@ -1,6 +1,41 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import CheckboxGroup from "react-checkbox-group";
 
 class ModalPopup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      name: "",
+      priority: "",
+      labelArr: [],
+      memberIDArr: [],
+      description: "",
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  memberChanged = (newMember) => {
+    this.setState({
+      memberIDArr: newMember,
+    });
+  };
+
+  labelChanged = (newLabel) => {
+    this.setState({
+      labelArr: newLabel,
+    });
+  };
+
   render() {
     return (
       <div className="modal fade" id="modalTask">
@@ -13,126 +48,114 @@ class ModalPopup extends Component {
                 ×
               </button>
             </div>
-            {/* Modal body */}
-            <div className="modal-body">
-              <div className="form-group">
-                <label htmlFor="taskName">Task Name:</label>
-                <input type="text" className="form-control" id="taskName" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="description">Description:</label>
-                <textarea
-                  className="form-control"
-                  rows={2}
-                  id="description"
-                  defaultValue={""}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="priority">Priority:</label>
-                <select className="form-control" id="priority">
-                  <option>Low</option>
-                  <option>Medium</option>
-                  <option>High</option>
-                </select>
-              </div>
-              <label>Person:</label>
-              <br />
-              <div className="form-check-inline">
-                <label className="form-check-label">
+            <form onSubmit={this.onSubmit}>
+              {/* Modal body */}
+              <div className="modal-body">
+                <div className="form-group">
+                  <label htmlFor="taskName">Task Name:</label>
                   <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    onChange={this.onChange}
                   />
-                  Nghĩa Văn
-                </label>
-              </div>
-              <div className="form-check-inline">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
+                </div>
+                <div className="form-group">
+                  <label htmlFor="description">Description:</label>
+                  <textarea
+                    className="form-control"
+                    rows={2}
+                    id="description"
+                    name="description"
+                    onChange={this.onChange}
                   />
-                  Minh Tuấn
-                </label>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="priority">Priority:</label>
+                  <select
+                    className="form-control"
+                    id="priority"
+                    name="priority"
+                    onChange={this.onChange}
+                  >
+                    <option value={-1}>Select...</option>
+                    <option value={3}>Low</option>
+                    <option value={2}>Medium</option>
+                    <option value={1}>High</option>
+                  </select>
+                </div>
+
+                <CheckboxGroup
+                  name="memberIDArr"
+                  value={this.state.memberIDArr}
+                  onChange={this.memberChanged}
+                >
+                  {(Checkbox) => (
+                    <>
+                      {" "}
+                      Person:
+                      <br />
+                      <label className="mr-3">
+                        <Checkbox value="user_2" /> Tuan
+                      </label>
+                      <label className="mr-3">
+                        <Checkbox value="user_3" /> Tiep
+                      </label>
+                      <label className="mr-3">
+                        <Checkbox value="user_4" /> Thuan
+                      </label>
+                      <label>
+                        <Checkbox value="user_5" /> Tien
+                      </label>
+                    </>
+                  )}
+                </CheckboxGroup>
+
+                <br />
+                <CheckboxGroup
+                  name="labelArr"
+                  value={this.state.labelArr}
+                  onChange={this.labelChanged}
+                >
+                  {(Checkbox) => (
+                    <>
+                      {" "}
+                      Label:
+                      <br />
+                      <label className="mr-3">
+                        <Checkbox value="Frontend" /> Frontend
+                      </label>
+                      <label className="mr-3">
+                        <Checkbox value="Backend" /> Backend
+                      </label>
+                      <label className="mr-3">
+                        <Checkbox value="API" /> API
+                      </label>
+                      <label>
+                        <Checkbox value="Issue" /> Issue
+                      </label>
+                    </>
+                  )}
+                </CheckboxGroup>
               </div>
-              <div className="form-check-inline">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
-                  />
-                  Trung Hiếu
-                </label>
+              {/* Modal footer */}
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  data-dismiss="modal"
+                >
+                  Add Task
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-dismiss="modal"
+                >
+                  Close
+                </button>
               </div>
-              <div className="form-check-inline">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
-                  />
-                  Tấn Khải
-                </label>
-              </div>
-              <br />
-              <br />
-              <label>Label:</label>
-              <br />
-              <div className="form-check-inline">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
-                  />
-                  Frontend
-                </label>
-              </div>
-              <div className="form-check-inline">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
-                  />
-                  Backend
-                </label>
-              </div>
-              <div className="form-check-inline">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
-                  />
-                  API
-                </label>
-              </div>
-              <div className="form-check-inline">
-                <label className="form-check-label">
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    defaultValue
-                  />
-                  Issue
-                </label>
-              </div>
-            </div>
-            {/* Modal footer */}
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-danger"
-                data-dismiss="modal"
-              >
-                Close
-              </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
